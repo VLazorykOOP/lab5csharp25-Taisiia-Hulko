1aca37649f4f339a53f0e64565557d9b60d49b0a
Taisiia Hulko <hulko.taisiia@chnu.edu.ua>
2025-04-30 13:04:58 +0300

    .

---
+++ 5.4/5.4/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Запит на вибір способу реалізації
+        Console.WriteLine("Як ви хочете реалізувати задачу?");
+        Console.WriteLine("1 - Використовувати структури");
+        Console.WriteLine("2 - Використовувати кортежі");
+        Console.WriteLine("3 - Використовувати записи");
+
+        int choice;
+        while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 3)
+        {
+            Console.WriteLine("Невірний вибір. Спробуйте ще раз.");
+        }
+
+        switch (choice)
+        {
+            case 1:
+                // Використовуємо структури
+                RunWithStructures();
+                break;
+            case 2:
+                // Використовуємо кортежі
+                RunWithTuples();
+                break;
+            case 3:
+                // Використовуємо записи
+                RunWithRecords();
+                break;
+            default:
+                Console.WriteLine("Невірний вибір.");
+                break;
+        }
+    }
+
+    // Реалізація через структури
+    static void RunWithStructures()
+    {
+        Console.WriteLine("Вибір: Структури");
+
+        // Введення масиву школярів
+        SchoolboyStruct[] schoolboys = new SchoolboyStruct[]
+        {
+            new SchoolboyStruct("Іван Іванов", "10-А", "123456789", 5, 4, 2, 4),
+            new SchoolboyStruct("Марія Петрівна", "10-Б", "987654321", 3, 4, 4, 4),
+            new SchoolboyStruct("Олег Олегович", "9-А", "555555555", 2, 3, 5, 5)
+        };
+
+        // Видалення школярів з оцінкою 2 хоча б по одному предмету
+        schoolboys = schoolboys.Where(s => s.MathGrade != 2 && s.PhysicsGrade != 2 && s.RussianGrade != 2 && s.LiteratureGrade != 2).ToArray();
+
+        // Додавання нового елемента на початок масиву
+        Array.Resize(ref schoolboys, schoolboys.Length + 1);
+        schoolboys[0] = new SchoolboyStruct("Анатолій Антонов", "11-А", "123123123", 4, 4, 3, 5);
+
+        // Виведення результатів
+        Console.WriteLine("Школярі після модифікації:");
+        foreach (var schoolboy in schoolboys)
+        {
+            Console.WriteLine($"Прізвище, ім'я, по батькові: {schoolboy.FullName}, Клас: {schoolboy.Class}, Телефон: {schoolboy.PhoneNumber}");
+            Console.WriteLine($"Оцінки: Математика: {schoolboy.MathGrade}, Фізика: {schoolboy.PhysicsGrade}, Російська мова: {schoolboy.RussianGrade}, Література: {schoolboy.LiteratureGrade}");
+            Console.WriteLine();
+        }
+    }
+
+    // Реалізація через кортежі
+    static void RunWithTuples()
+    {
+        Console.WriteLine("Вибір: Кортежі");
+
+        // Введення масиву школярів у вигляді кортежів
+        var schoolboys = new (string FullName, string Class, string PhoneNumber, int MathGrade, int PhysicsGrade, int RussianGrade, int LiteratureGrade)[]
+        {
+            ("Іван Іванов", "10-А", "123456789", 5, 4, 2, 4),
+            ("Марія Петрівна", "10-Б", "987654321", 3, 4, 4, 4),
+            ("Олег Олегович", "9-А", "555555555", 2, 3, 5, 5)
+        };
+
+        // Видалення школярів з оцінкою 2 хоча б по одному предмету
+        schoolboys = schoolboys.Where(s => s.MathGrade != 2 && s.PhysicsGrade != 2 && s.RussianGrade != 2 && s.LiteratureGrade != 2).ToArray();
+
+        // Додавання нового елемента на початок масиву
+        var newSchoolboy = ("Анатолій Антонов", "11-А", "123123123", 4, 4, 3, 5);
+        schoolboys = new[] { newSchoolboy }.Concat(schoolboys).ToArray();
+
+        // Виведення результатів
+        Console.WriteLine("Школярі після модифікації:");
+        foreach (var schoolboy in schoolboys)
+        {
+            Console.WriteLine($"Прізвище, ім'я, по батькові: {schoolboy.FullName}, Клас: {schoolboy.Class}, Телефон: {schoolboy.PhoneNumber}");
+            Console.WriteLine($"Оцінки: Математика: {schoolboy.MathGrade}, Фізика: {schoolboy.PhysicsGrade}, Російська мова: {schoolboy.RussianGrade}, Література: {schoolboy.LiteratureGrade}");
+            Console.WriteLine();
+        }
+    }
+
+    // Реалізація через записи
+    static void RunWithRecords()
+    {
+        Console.WriteLine("Вибір: Записи");
+
+        // Введення масиву школярів у вигляді записів
+        var schoolboys = new[]
+        {
+            new SchoolboyRecord("Іван Іванов", "10-А", "123456789", 5, 4, 2, 4),
+            new SchoolboyRecord("Марія Петрівна", "10-Б", "987654321", 3, 4, 4, 4),
+            new SchoolboyRecord("Олег Олегович", "9-А", "555555555", 2, 3, 5, 5)
+        };
+
+        // Видалення школярів з оцінкою 2 хоча б по одному предмету
+        schoolboys = schoolboys.Where(s => s.MathGrade != 2 && s.PhysicsGrade != 2 && s.RussianGrade != 2 && s.LiteratureGrade != 2).ToArray();
+
+        // Додавання нового елемента на початок масиву
+        var newSchoolboy = new SchoolboyRecord("Анатолій Антонов", "11-А", "123123123", 4, 4, 3, 5);
+        schoolboys = new[] { newSchoolboy }.Concat(schoolboys).ToArray();
+
+        // Виведення результатів
+        Console.WriteLine("Школярі після модифікації:");
+        foreach (var schoolboy in schoolboys)
+        {
+            Console.WriteLine($"Прізвище, ім'я, по батькові: {schoolboy.FullName}, Клас: {schoolboy.Class}, Телефон: {schoolboy.PhoneNumber}");
+            Console.WriteLine($"Оцінки: Математика: {schoolboy.MathGrade}, Фізика: {schoolboy.PhysicsGrade}, Російська мова: {schoolboy.RussianGrade}, Література: {schoolboy.LiteratureGrade}");
+            Console.WriteLine();
+        }
+    }
+}
+
+// Структура для варіанту з використанням структур
+struct SchoolboyStruct
+{
+    public string FullName;
+    public string Class;
+    public string PhoneNumber;
+    public int MathGrade;
+    public int PhysicsGrade;
+    public int RussianGrade;
+    public int LiteratureGrade;
+
+    public SchoolboyStruct(string fullName, string className, string phoneNumber, int mathGrade, int physicsGrade, int russianGrade, int literatureGrade)
+    {
+        FullName = fullName;
+        Class = className;
+        PhoneNumber = phoneNumber;
+        MathGrade = mathGrade;
+        PhysicsGrade = physicsGrade;
+        RussianGrade = russianGrade;
+        LiteratureGrade = literatureGrade;
+    }
+}
+
+// Запис для варіанту з використанням записів
+record SchoolboyRecord(string FullName, string Class, string PhoneNumber, int MathGrade, int PhysicsGrade, int RussianGrade, int LiteratureGrade);
--- Lab5CSharp/Program.cs
+++ Lab5CSharp/Program.cs
@@ -1,58 +1,285 @@
-﻿// See https://aka.ms/new-console-template for more information
-/// <summary>
-///  Top-level statements 
-///  Код програми (оператори)  вищого рівня
-/// </summary>
-///
-Console.WriteLine("Lab5 C# ");
-AnyFunc();
+﻿using System;
+using System.Collections.Generic;
 
-/// <summary>
-/// 
-///  Top-level statements must precede namespace and type declarations.
-/// At the top-level methods/functions can be defined and used
-/// На верхньому рівні можна визначати та використовувати методи/функції
-/// </summary>
-void AnyFunc()
+class Program
 {
-    Console.WriteLine(" Some function in top-level");
-}
-Console.WriteLine("Problems 1 ");
-AnyFunc();
-//  приклад класів
-UserClass cl = new UserClass();
-cl.Name = " UserClass top-level ";
-User.UserClass cl2 = new();
-cl2.Name = " UserClass namespace User ";
-
-
-
-
-/// <summary>
-/// 
-/// Top-level statements must precede namespace and type declarations.
-/// Оператори верхнього рівня мають передувати оголошенням простору імен і типу.
-/// Створення класу(ів) або оголошенням простору імен є закіченням  іструкцій верхнього рівня
-/// 
-/// </summary>
-
-namespace User
-{
-    class UserClass
+    static void Main()
     {
-        public string Name { get; set; }
-       public  UserClass()
+        Console.WriteLine("Виберіть завдання:");
+        Console.WriteLine("1 - Завдання 1 та 2");
+        Console.WriteLine("3 - Завдання 3");
+        int choice = int.Parse(Console.ReadLine());
+
+        switch (choice)
         {
-            Name = "NoName";
-        }
-        UserClass(string n)
-        {
-            Name = n;
+            case 1:
+                // Завдання 1 та 2
+                RunTask1And2();
+                break;
+
+            case 2:
+                // Завдання 1 та 2
+                RunTask1And2();
+                break;
+
+            case 3:
+                // Завдання 3
+                RunTask3();
+                break;
+
+            default:
+                Console.WriteLine("Невірний вибір.");
+                break;
         }
     }
 
+    // Завдання 1 та 2
+    static void RunTask1And2()
+    {
+        Console.WriteLine("Завдання 1 та 2");
+
+        // Завдання 1: Ієрархія класів
+        // Замінити створення PrintedEdition на конкретний клас (наприклад, Journal)
+        PrintedEdition printedEdition = new Journal("Журнал наука", 2025, "Іван Іванов");
+        printedEdition.Show();
+        Console.WriteLine();
+
+        Journal journal = new Journal("Технічний журнал", 2023, "Іван Іванов");
+        journal.Show();
+        Console.WriteLine();
+
+        Book book = new Book("Математика для школярів", 2021, "Олексій Петренко");
+        book.Show();
+        Console.WriteLine();
+
+        Textbook textbook = new Textbook("Основи фізики", 2020, "Михайло Шевченко", "Фізика");
+        textbook.Show();
+        Console.WriteLine();
+
+        // Завдання 2: Конструктори і деструктори
+        Console.WriteLine("Виклик деструкторів:");
+        GC.Collect();  // Змушує збирач сміття спрацювати та викликати деструктори
+    }
+
+    // Завдання 3
+    static void RunTask3()
+    {
+        Console.WriteLine("Завдання 3");
+
+        List<Product> products = new List<Product>();
+
+        // Створення товарів
+        Product product1 = new ProductItem("Молоко", 20.5m, new DateTime(2025, 3, 1), new DateTime(2025, 4, 10));
+        Product product2 = new Batch("Пакет цукру", 15.0m, new DateTime(2025, 2, 15), new DateTime(2025, 5, 10), 100);
+        Product product3 = new Set("Набір для чаю", 50.0m, new DateTime(2025, 1, 20), new DateTime(2025, 6, 1), new List<Product> { product1, product2 });
+
+        // Додавання товарів в масив
+        products.Add(product1);
+        products.Add(product2);
+        products.Add(product3);
+
+        DateTime currentDate = DateTime.Now;
+
+        Console.WriteLine("Всі товари:");
+        foreach (var product in products)
+        {
+            product.Show();
+            Console.WriteLine($"Прострочений: {product.IsExpired(currentDate)}");
+            Console.WriteLine();
+        }
+
+        // Пошук прострочених товарів
+        Console.WriteLine("Прострочені товари:");
+        foreach (var product in products)
+        {
+            if (product.IsExpired(currentDate))
+            {
+                product.Show();
+            }
+        }
+    }
 }
-class UserClass
+
+abstract class PrintedEdition
+{
+    public string Title { get; set; }
+    public int Year { get; set; }
+
+    public PrintedEdition() { }
+    public PrintedEdition(string title, int year)
+    {
+        Title = title;
+        Year = year;
+        Console.WriteLine("Створено PrintedEdition");
+    }
+
+    public virtual void Show()
+    {
+        Console.WriteLine($"Назва: {Title}, Рік видання: {Year}");
+    }
+
+    ~PrintedEdition()
+    {
+        Console.WriteLine("Знищено PrintedEdition");
+    }
+}
+
+class Journal : PrintedEdition
+{
+    public string Editor { get; set; }
+
+    public Journal() : base() { }
+    public Journal(string title, int year, string editor) : base(title, year)
+    {
+        Editor = editor;
+        Console.WriteLine("Створено Journal");
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Редактор: {Editor}");
+    }
+
+    ~Journal()
+    {
+        Console.WriteLine("Знищено Journal");
+    }
+}
+
+class Book : PrintedEdition
+{
+    public string Author { get; set; }
+
+    public Book() : base() { }
+    public Book(string title, int year, string author) : base(title, year)
+    {
+        Author = author;
+        Console.WriteLine("Створено Book");
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Автор: {Author}");
+    }
+
+    ~Book()
+    {
+        Console.WriteLine("Знищено Book");
+    }
+}
+
+class Textbook : Book
+{
+    public string Subject { get; set; }
+
+    public Textbook() : base() { }
+    public Textbook(string title, int year, string author, string subject) : base(title, year, author)
+    {
+        Subject = subject;
+        Console.WriteLine("Створено Textbook");
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Предмет: {Subject}");
+    }
+
+    ~Textbook()
+    {
+        Console.WriteLine("Знищено Textbook");
+    }
+}
+
+abstract class Product
 {
     public string Name { get; set; }
+    public decimal Price { get; set; }
+    public DateTime ProductionDate { get; set; }
+    public DateTime ExpiryDate { get; set; }
+
+    public abstract void Show();
+    public abstract bool IsExpired();
+
+    // Метод для перевірки терміну придатності
+    public bool IsExpired(DateTime currentDate)
+    {
+        return currentDate > ExpiryDate;
+    }
+}
+
+class ProductItem : Product
+{
+    public ProductItem(string name, decimal price, DateTime productionDate, DateTime expiryDate)
+    {
+        Name = name;
+        Price = price;
+        ProductionDate = productionDate;
+        ExpiryDate = expiryDate;
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine($"Продукт: {Name}, Ціна: {Price} грн, Дата виробництва: {ProductionDate.ToShortDateString()}, Термін придатності: {ExpiryDate.ToShortDateString()}");
+    }
+
+    public override bool IsExpired()
+    {
+        return DateTime.Now > ExpiryDate;
+    }
+}
+
+class Batch : Product
+{
+    public int Quantity { get; set; }
+
+    public Batch(string name, decimal price, DateTime productionDate, DateTime expiryDate, int quantity)
+    {
+        Name = name;
+        Price = price;
+        ProductionDate = productionDate;
+        ExpiryDate = expiryDate;
+        Quantity = quantity;
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine($"Партія: {Name}, Ціна: {Price} грн, Кількість: {Quantity} шт, Дата виробництва: {ProductionDate.ToShortDateString()}, Термін придатності: {ExpiryDate.ToShortDateString()}");
+    }
+
+    public override bool IsExpired()
+    {
+        return DateTime.Now > ExpiryDate;
+    }
+}
+
+class Set : Product
+{
+    public List<Product> Products { get; set; }
+
+    public Set(string name, decimal price, DateTime productionDate, DateTime expiryDate, List<Product> products)
+    {
+        Name = name;
+        Price = price;
+        ProductionDate = productionDate;
+        ExpiryDate = expiryDate;
+        Products = products;
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine($"Комплект: {Name}, Ціна: {Price} грн, Дата виробництва: {ProductionDate.ToShortDateString()}, Термін придатності: {ExpiryDate.ToShortDateString()}");
+        Console.WriteLine("Склад комплекту:");
+        foreach (var product in Products)
+        {
+            product.Show();
+        }
+    }
+
+    public override bool IsExpired()
+    {
+        return DateTime.Now > ExpiryDate;
+    }
 }
